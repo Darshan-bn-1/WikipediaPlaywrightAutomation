483a5ce66f85ef8e26edaa87ff22e94faf4994c9
Darshan B N <darshanbn@clarivate.com>
2026-09-24 06:37:30 +0530

    Added Winston logging

--- tests/sports.spec.ts
+++ tests/sports.spec.ts
@@ -1,20 +1,30 @@
 import { test } from '@playwright/test';
 import { WikipediaPage } from '../pages/WikipediaPage';
+import { logger } from '../utils/logger';
 
 test('Verify Wikipedia Sport Page Heading', async ({ page }) => {
+
+    logger.info('Test Started');
 
     const wikipediaPage = new WikipediaPage(page);
 
     await wikipediaPage.openSportPage();
+    logger.info('Opened Sport Page');
 
     await wikipediaPage.verifyPageLoaded();
+    logger.info('Page Loaded Successfully');
 
     await wikipediaPage.verifyHeading();
+    logger.info('Heading Verified');
 
     await wikipediaPage.verifyImagePresent();
+    logger.info('Image Verification Completed');
 
     await wikipediaPage.takeScreenshot();
+    logger.info('Screenshot Captured');
 
     await wikipediaPage.performVisualValidation();
+    logger.info('Visual Validation Completed');
 
+    logger.info('Test Passed');
 });
